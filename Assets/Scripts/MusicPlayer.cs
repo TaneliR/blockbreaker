99519cc22f466fd52b24e38596f70e2ced77fb9d
TaneliR <marcu@dnainternet.net>
2017-01-31 11:09:34 +0200

    First commit, few scenes and sprites added. Also few scripts handling level management (legacy) and music player instance

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static MusicPlayer instance = null;
+	// Use this for initialization
+    void Awake ()
+    {
+        Debug.Log ("Music player Awake " + GetInstanceID());
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-dustructing!");
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+	void Start () {
+        Debug.Log("Music player Start " + GetInstanceID());
+    }
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+}
