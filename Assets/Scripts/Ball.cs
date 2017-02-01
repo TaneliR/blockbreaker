0684a461d9287c9950dff1a87244401ab74be1b9
TaneliR <marcu@dnainternet.net>
2017-02-02 01:29:08 +0200

    MOD some scenes, tidying up

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
-    private Paddle paddle;
 
+    private Rigidbody2D rigidBody;
     private bool gameStarted = false;
+    private Paddle paddle;
     private Vector3 paddleToBallVector;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
         paddle = GameObject.FindObjectOfType<Paddle>();
         paddleToBallVector = this.transform.position - paddle.transform.position;
         
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if (!gameStarted)        
         {
             // Locked the ball relative to the paddle.
@@ -25,8 +29,13 @@ public class Ball : MonoBehaviour {
             {
                 // Stop holding ball when mouse is clicked.
                 gameStarted = true;
-                GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10f);
+                rigidBody.velocity = new Vector2(Random.Range(-0.4f, 0.4f), Random.Range(10f, 12f));
             }
         }       
 	}
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        Vector2 tweak = new Vector2(Random.Range(0f, 0.4f), Random.Range(0f, 3f));
+        rigidBody.velocity += tweak;
+    }
 }
